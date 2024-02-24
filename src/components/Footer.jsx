f9b5cf1071366752142599b311b0c7955c8789b8
sachinrajput8929 <sachinrajput8929@gmail.com>
2024-02-24 14:05:50 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import logo from "./pic/man.png"
+//footer
+const Footer = () => {
+  return (
+    <>
+      
+        <div class="shadow-lg p-3 mb-5 bg-white rounded">
+        <div className='conatiner text-center  '>
+          {/* <div className='img'>
+            <img src={logo} alt='' />
+          </div> */}
+          <p>© 2024. All rights reserved by Sachin Portfolio</p>
+        </div>
+        </div>
+       
+      
+    </>
+  )
+}
+
+export default Footer
